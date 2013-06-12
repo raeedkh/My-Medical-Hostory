f47225007148a5766976fc79dfa7531f78a1b9c5
Raeed Alkhalifa <raeed.khalifa@gmail.com>
2013-06-12 05:30:49 -0700

    Added printDbToLog function in TreeActivity and Added InsertTestDataIntoDb in TreeActivity

--- MyMedicalHistory/src/edu/cgu/ist380/alkhalir/mymedicalhistory/TreeActivity.java
+++ MyMedicalHistory/src/edu/cgu/ist380/alkhalir/mymedicalhistory/TreeActivity.java
@@ -1,10 +1,16 @@
 package edu.cgu.ist380.alkhalir.mymedicalhistory;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import edu.cgu.ist380.alkhalir.mymedicalhistory.db.Condition;
+import edu.cgu.ist380.alkhalir.mymedicalhistory.db.ConditionsDataSource;
 import edu.cgu.ist380.alkhalir.mymedicalhistory.db.Person;
+import edu.cgu.ist380.alkhalir.mymedicalhistory.db.PersonsDataSource;
 
 public class TreeActivity extends Activity {
 
@@ -32,8 +38,6 @@ public class TreeActivity extends Activity {
 		motherFrag.setPerson(mother);
 		PersonFragment fatherFrag = (PersonFragment)fragmentMgr.findFragmentById(R.id.fatherFragment);
 		fatherFrag.setPerson(father);
-		
-		
 	}
 
 	@Override
@@ -43,4 +47,69 @@ public class TreeActivity extends Activity {
 		return true;
 	}
 
+	//Method to print DB content to Log
+	public void printDBtoLog()
+	{
+		PersonsDataSource personDS = new PersonsDataSource(this);
+		ConditionsDataSource conditionDS= new ConditionsDataSource(this);
+		
+		personDS.open();
+		conditionDS.open();
+		
+		Log.i(this.getClass().getName(), "Person DB is: "+ personDS);
+		Log.i(this.getClass().getName(), "Condition DB is: "+ conditionDS);		
+		
+		List<Person> persons=personDS.getAllPersons();
+		List<Condition> conditions=conditionDS.getAllConditions();
+		
+		
+		
+		int i=0;
+		while (i<persons.size())
+		{
+			Log.i(this.getClass().getName(), persons.get(i).toString());
+			i++;
+		}
+
+		i=0;
+		while (i<conditions.size())
+		{
+			Log.i(this.getClass().getName(), conditions.get(i).toString());
+			i++;
+		}
+		personDS.close();
+		conditionDS.close();		
+	}
+
+	//Method to insert test data into the DB
+	public void insertTestData()
+	{
+		PersonsDataSource personDS = new PersonsDataSource(this);
+		ConditionsDataSource conditionDS= new ConditionsDataSource(this);
+		
+		personDS.open();
+		conditionDS.open();
+		
+		Log.i(this.getClass().getName(), "Person DB is: "+ personDS);
+		Log.i(this.getClass().getName(), "Condition DB is: "+ conditionDS);		
+		
+		Person testPerson=new Person();
+		testPerson.setBirthDate("04/15/1937");
+		testPerson.setFirstName("Jenna");
+		testPerson.setLastName("Antony");
+		testPerson.setRelationship("Mother");
+		testPerson.setGender("Female");
+		testPerson=personDS.createPerson(testPerson);
+		
+		Condition testCondition=new Condition();
+		testCondition.setDateAcquired("06/03/2005");
+		testCondition.setDescription("Diabeties");
+		testCondition.setPersonId(testPerson.getId());
+		testCondition.setRemarks("Very mild");
+		testCondition=conditionDS.createCondition(testCondition);		
+
+		personDS.close();
+		conditionDS.close();
+		
+	}	
 }
